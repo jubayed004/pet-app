aae581083a82a93094b1bbe36c60bf132b929fed
jubayed004 <mohammadjubayed.islam97@gmail.com>
2025-05-26 15:42:27 +0600

    some change

---
+++ lib/presentation/screens/vendor_selection/vendor_selection_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:pet_app/presentation/components/custom_image/custom_image.dart';
+
+class VendorSelectionScreen extends StatelessWidget {
+  const VendorSelectionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
