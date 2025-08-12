dfc1cd31923641c9bebe08cd5abaa4da854e381b
jubayed004 <mohammadjubayed.islam97@gmail.com>
2025-08-12 10:25:28 +0600

    Testing contribution

--- lib/presentation/screens/business_owners/business_all_pets/view/business_pet_details_screen.dart
+++ lib/presentation/screens/business_owners/business_all_pets/view/business_pet_details_screen.dart
@@ -276,7 +276,6 @@ class _BusinessPetsDetailsScreenState extends State<BusinessPetsDetailsScreen> {
                         dateOfMonth: DateFormat("dd MMMM yyyy").format(item.treatmentDate?.toLocal() ?? DateTime.now()),
                         drName: item.doctorName?? "",
                         status: item.treatmentStatus?? "",
-
                         statusColor: Colors.green, id: item.id ?? "",
                       );
                     }
