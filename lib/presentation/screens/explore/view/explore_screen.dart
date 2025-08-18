eb89532af3648ebda35f540a5e59ced5aca5e644
jubayed004 <mohammadjubayed.islam97@gmail.com>
2025-08-18 14:37:33 +0600

    socket service create

--- lib/presentation/screens/explore/view/explore_screen.dart
+++ lib/presentation/screens/explore/view/explore_screen.dart
@@ -4,9 +4,22 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:pet_app/presentation/components/custom_button/custom_defualt_appbar.dart';
 import 'package:pet_app/presentation/components/custom_image/custom_image.dart';
 
-class ExploreScreen extends StatelessWidget {
+class ExploreScreen extends StatefulWidget {
   const ExploreScreen({super.key});
 
+  @override
+  State<ExploreScreen> createState() => _ExploreScreenState();
+}
+
+late GoogleMapController mapController;
+
+const LatLng _center = LatLng(23.804693584341365, 90.41590889596907);
+
+void _onMapCreated(GoogleMapController controller) {
+  mapController = controller;
+}
+
+class _ExploreScreenState extends State<ExploreScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,18 +31,24 @@ class ExploreScreen extends StatelessWidget {
           SliverToBoxAdapter(
             child: Column(
               children: [
-            /*    CustomImage(imageSrc: "assets/images/map.png", boxFit: BoxFit.cover),*/
-                GoogleMap(
-
-
-                    initialCameraPosition: CameraPosition(target: LatLng(23.804693584341365, 90.41590889596907, ),zoom: 14)),
+                // Uncomment this if you need the image
+                /*CustomImage(imageSrc: "assets/images/map.png", boxFit: BoxFit.cover),*/
+                SizedBox(
+                  height: 300,  // Set a fixed height for the map
+                  child: GoogleMap(
+                    onMapCreated: _onMapCreated,
+                    initialCameraPosition: CameraPosition(
+                      target: _center,
+                      zoom: 14,
+                    ),
+                  ),
+                ),
                 const Gap(10),
               ],
             ),
           )
         ],
-      )
+      ),
     );
-
   }
 }
---
+++ lib/service/socket_service.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:pet_app/helper/local_db/local_db.dart';
+import 'package:socket_io_client/socket_io_client.dart' as io;
+import 'api_url.dart';
+
+class SocketApi {
+  factory SocketApi() {
+    return _socketApi;
+  }
+
+  SocketApi._internal();
+
+  static io.Socket? _socket;
+  static io.Socket? get socket => _socket;
+
+  ///<------------------------- Socket Initialization with dynamic User ID ---------------->
+
+  static Future<void> init() async {
+    if (_socket != null && _socket!.connected) {
+      debugPrint('Socket is already initialized and connected.');
+      return;
+    }
+
+    String userId = await DBHelper().getUserId();
+    if (userId.isEmpty || userId == "null") {
+      debugPrint('Socket Connected >>>>>>>>>>>> FALSE userId.isEmpty <<<<<<<<<<<<');
+      return;
+    }
+
+    _socket = io.io(
+      ApiUrl.socketUrl(userID: userId),
+      io.OptionBuilder()
+          .setTransports(['websocket'])
+          .enableAutoConnect()
+          .setReconnectionDelay(2000)
+          .setReconnectionAttempts(0)
+          .setTimeout(5000)
+          .build(),
+    );
+
+    debugPrint('$userId=============> Socket initialization, connected: ${_socket?.connected}');
+
+    // Listen for socket connection
+    _socket?.onConnect((_) {
+      _socket?.on('pong', (data) {
+        debugPrint('Received pong from server');
+      });
+      debugPrint('==============>>>>>>> Socket Connected ${_socket?.connected} ===============<<<<<<<');
+    });
+
+    _socket?.on('unauthorized', (dynamic data) {
+      debugPrint('Unauthorized');
+    });
+
+    // Listen for errors
+    _socket?.onError((dynamic error) {
+      debugPrint('Socket error: $error');
+    });
+
+    // Listen for disconnection
+    _socket?.onDisconnect((dynamic data) {
+      debugPrint('>>>>>>>>>> Socket instance disconnected <<<<<<<<<<<<$data');
+      reconnect();
+    });
+
+    _socket?.on('pong', (data) {
+      debugPrint('Received pong from server');
+    });
+  }
+
+  static void reconnect() {
+    if (_socket != null && !_socket!.connected) {
+      debugPrint('Attempting to reconnect...');
+      _socket?.connect();
+    }
+  }
+
+  static final SocketApi _socketApi = SocketApi._internal();
+}
